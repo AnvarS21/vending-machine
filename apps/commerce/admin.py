47ad2a7e32e08758cb6b6d0702ac5e956014b675
AShamsudinov <AShamsudinov@beeline.kg>
2025-02-26 16:03:37 +0600

    configuring admin panel

--- apps/commerce/admin.py
+++ apps/commerce/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Transaction
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity')
+    search_fields = ('name',)
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'quantity', 'deposit', 'change', 'amount', 'created_dt', 'status')
+    list_filter = ('created_dt',)
+    search_fields = ('product_name',)
+    date_hierarchy = 'created_dt'

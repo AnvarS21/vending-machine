1b1ff786d08a05eb1d4a085381c308129a61b85e
AShamsudinov <AShamsudinov@beeline.kg>
2025-02-27 14:50:12 +0600

    added and configurate docker, nginx, scripts

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+chmod +x /app/scripts/entrypoint.sh
+set -e
+set -x
+
+python manage.py migrate
+
+python manage.py loaddata products
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+
+
+python manage.py test
+
+python manage.py collectstatic --noinput
+
+exec gunicorn --bind 0.0.0.0:8000 --workers 3 core.wsgi:application

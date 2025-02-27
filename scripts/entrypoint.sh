5f674796f83271f2b7f2d0e29a2285d8863509ba
AShamsudinov <AShamsudinov@beeline.kg>
2025-02-27 16:04:12 +0600

    fixed nginx, script and csrf

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,6 +9,7 @@ python manage.py loaddata products
 
 echo "from django.contrib.auth import get_user_model; \
 User = get_user_model(); \
+User.objects.filter(username='$DJANGO_SUPERUSER_USERNAME').delete(); \
 User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
 
 

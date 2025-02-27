5c463a9e0351de7a3bc8c05e19f3f218947999a4
AShamsudinov <AShamsudinov@beeline.kg>
2025-02-27 11:00:54 +0600

    fixed urls and added tests

--- apps/commerce/urls.py
+++ apps/commerce/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from apps.commerce.views import ProductListView, TransactionViewSet
 
 router = DefaultRouter()
-router.register(r'products', ProductListView)
+router.register(r'products', ProductListView, basename='products')
 router.register(r'vending', TransactionViewSet, basename='vending')
 
 urlpatterns = [
